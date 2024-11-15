c3f2c0fe4add09057b6235b0b75e850925903172
Gabriel Chaldu <gabrielchaldu@gmail.com>
2024-11-15 16:44:10 -0300

    feat: modificando el request mapping

--- src/main/java/net/youtics/backend/crudProductos/controllers/ProductoController.java
+++ src/main/java/net/youtics/backend/crudProductos/controllers/ProductoController.java
@@ -11,17 +11,17 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/productos")
 public class ProductoController {
     @Autowired
     ProductoServiceManager productoServiceManager;
 
-    @GetMapping("/productos")
+    @GetMapping("/")
     public List<Producto> listarProductos(){
         return this.productoServiceManager.get();
     }
 
-    @GetMapping("/productos/{id}")
+    @GetMapping("/{id}")
     public Producto optenerProductoId( @PathVariable Long id){
         return this.productoServiceManager.getById(id).get();
     }
@@ -35,13 +35,13 @@ public class ProductoController {
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping("/productos")
+    @PostMapping("/")
     public ResponseEntity<Producto> save(@RequestBody Producto producto){
         Producto newProducto = productoServiceManager.save(producto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newProducto);
     }
 
-    @DeleteMapping("/productos/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Producto> deleteProducto(@PathVariable Long id){
         Optional<Producto> optionalProducto = this.productoServiceManager.delete(id);
 
@@ -51,7 +51,7 @@ public class ProductoController {
         return ResponseEntity.notFound().build();
     }
 
-    @PutMapping("/productos/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Producto> updateProducto(@RequestBody Producto producto,
                                                    @PathVariable Long id)
     {
